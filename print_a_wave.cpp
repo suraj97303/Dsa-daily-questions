63e5f1e7edea4c73a0963a03351e6e68a3f236af
Suraj Mishra <91696589+suraj97303@users.noreply.github.com>
2023-02-08 04:35:05 +0530

    Create print_a_wave.cpp

---
+++ print_a_wave.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std;
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    for(int col=0; col<mCols; col++){
+
+        if(col&1)
+        {
+            // odd index bottom to top
+            for(int row =nRows-1; row>=0; row--){
+                // cout<<arr[row][col]<<" ";
+                ans.push_back(arr[row][col]);
+            }
+        }
+        else{
+                for(int row =0; row<nRows; row++){
+                    // cout<<arr[row][col]<<" ";
+                         ans.push_back(arr[row][col]);
+                }
+            // top to bottom
+
+        }
+        
+    }
+return ans;
+}
