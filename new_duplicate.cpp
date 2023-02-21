bff593c47bee50c7335a7fb2c6f9b8bacf42124b
Suraj Mishra <91696589+suraj97303@users.noreply.github.com>
2023-02-21 17:31:26 +0530

    Create new_duplicate.cpp

---
+++ new_duplicate.cpp
@@ -0,0 +1,19 @@
+ Single Element in a Sorted Array
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+       unordered_map<int,int> mp;
+       for(auto i:nums)
+       mp[i]++;
+       
+       for(auto x:mp)
+       {
+           if(x.second==1){
+               return x.first;
+           }
+       }
+       return -1;
+        
+    }
+};
