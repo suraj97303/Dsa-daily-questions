2149620c627e02324dff21d04580263e2db966dd
Suraj Mishra <91696589+suraj97303@users.noreply.github.com>
2023-03-22 20:52:56 +0530

    Create Binary_tree_postorder.cpp

---
+++ Binary_tree_postorder.cpp
@@ -0,0 +1,45 @@
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void solve(TreeNode* root, vector<int>&ans){
+    if(root==NULL){
+        return;
+    }
+    solve(root->left,ans);
+    solve(root->right,ans);
+    ans.push_back(root->val);
+}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
